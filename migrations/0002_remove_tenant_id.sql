f4462930c9ab8b5c477bd3f75dd93bb5a7e3cc07
Casey Brooks <casey.brooks@agyn.io>
2026-03-22 00:43:54 +0000

    chore: update buf lock and migration

--- migrations/0002_remove_tenant_id.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS idx_managed_identities_tenant;
-DROP INDEX IF EXISTS idx_managed_identities_type_tenant;
-
-ALTER TABLE managed_identities DROP COLUMN tenant_id;
-
-CREATE INDEX IF NOT EXISTS idx_managed_identities_type ON managed_identities (identity_type);
