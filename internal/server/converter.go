977d3ad2ccdfe96f28ed71fb25f3a41132fad741
casey-brooks <casey.brooks@agyn.io>
2026-03-24 23:33:19 +0000

    fix(proto): drop managed identity hack (#18)

--- internal/server/converter.go
+++ internal/server/converter.go
@@ -6,13 +6,10 @@ import (
 	identityv1 "github.com/agynio/ziti-management/.gen/go/agynio/api/identity/v1"
 	zitimanagementv1 "github.com/agynio/ziti-management/.gen/go/agynio/api/ziti_management/v1"
 	"github.com/google/uuid"
-	"google.golang.org/protobuf/encoding/protowire"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/agynio/ziti-management/internal/store"
 )
-
-const managedIdentityZitiServiceIDFieldNumber = 4
 
 func parseUUID(value string) (uuid.UUID, error) {
 	if value == "" {
